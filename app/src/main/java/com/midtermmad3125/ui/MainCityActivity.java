f48e680ff18991bb31e21e19063c5a224af4e247
amritpalsingh <amritpalsingh3027@.com>
2019-04-08 19:26:11 -0400

    text views added in main acitivity

--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -3,17 +3,26 @@ package com.midtermmad3125.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.midtermmad3125.R;
 import com.midtermmad3125.utils.ReadJSONUtils;
 
 public class MainCityActivity extends AppCompatActivity
-{
 
+{
+private TextView cname;
+private TextView clat;
+private TextView clon;
+private TextView cpopulation;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cname = findViewById(R.id.txtcityname);
+        clat = findViewById(R.id.txtcitylat);
+        clon = findViewById(R.id.txtcitylon);
+        cpopulation = findViewById(R.id.txtcityp);
     }
 }
