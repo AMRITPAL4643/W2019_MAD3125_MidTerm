4aafc6f121b008972199a461b229e2c80ce59bbb
amritpalsingh <amritpalsingh3027@.com>
2019-04-08 19:31:04 -0400

    coordinate class added

--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -3,6 +3,7 @@ package com.midtermmad3125.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.midtermmad3125.R;
@@ -15,6 +16,7 @@ private TextView cname;
 private TextView clat;
 private TextView clon;
 private TextView cpopulation;
+private Button w;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,5 +26,6 @@ private TextView cpopulation;
         clat = findViewById(R.id.txtcitylat);
         clon = findViewById(R.id.txtcitylon);
         cpopulation = findViewById(R.id.txtcityp);
+        w = findViewById(R.id.btnweather);
     }
 }
