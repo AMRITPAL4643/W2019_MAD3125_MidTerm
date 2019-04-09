9894b7e65e11e9d73e4145861751399af5a5e34c
amritpalsingh <amritpalsingh3027@.com>
2019-04-08 20:47:18 -0400

    wheather class code added

--- app/src/main/java/com/midtermmad3125/Modal/Wheather.java
+++ app/src/main/java/com/midtermmad3125/Modal/Wheather.java
@@ -1,6 +1,42 @@
 package com.midtermmad3125.Modal;
 
-public class Wheather
-{
+public class Wheather {
+
+    private Integer id;
+    private String main;
+    private String description;
+    private String icon;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
 
 }
